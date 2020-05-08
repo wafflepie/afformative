cbf609b588bf808a9c1dc5509c47bfb0b788737d
Václav Jančařík <vaclav.janc@gmail.com>
2020-05-08 16:32:42 +0200

    initial repo setup

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  root: true,
+  extends: ["airbnb", "airbnb/hooks", "prettier", "prettier/react"],
+  plugins: ["react-hooks"],
+  rules: {
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: ["block", "block-like", "export", "import", "multiline-expression"],
+        next: "*",
+      },
+      {
+        blankLine: "always",
+        prev: "*",
+        next: ["block", "block-like", "export", "import", "return", "throw"],
+      },
+      {
+        blankLine: "any",
+        prev: ["export", "import"],
+        next: ["export", "import"],
+      },
+      {
+        blankLine: "never",
+        prev: "case",
+        next: "*",
+      },
+    ],
+    // NOTE: Sadly, `sort-imports` is not really compatible with `import/order`.
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: false,
+        },
+      },
+    ],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "no-console": [
+      "error",
+      {
+        allow: ["warn", "error", "info"],
+      },
+    ],
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: ["packages/**/*.test.js", "packages/**/*.config.js", "*.config.js"],
+      },
+    ],
+    "import/prefer-default-export": "off",
+  },
+}
---
+++ rollup.config.js
@@ -0,0 +1,110 @@
+import path from "path"
+
+import { split, compose, join, prepend, tail, map } from "ramda"
+import { toPascalCase, toKebabCase } from "ramda-extension"
+import autoExternal from "rollup-plugin-auto-external"
+import babelPlugin from "rollup-plugin-babel"
+import cjsPlugin from "rollup-plugin-commonjs"
+import nodeResolvePlugin from "rollup-plugin-node-resolve"
+import replace from "rollup-plugin-replace"
+import { terser as terserPlugin } from "rollup-plugin-terser"
+
+const { LERNA_ROOT_PATH } = process.env
+
+const plugins = {
+  cjs: cjsPlugin({
+    include: /node_modules/,
+  }),
+  terser: terserPlugin({
+    compress: {
+      pure_getters: true,
+      unsafe: true,
+      unsafe_comps: true,
+      warnings: false,
+    },
+  }),
+  nodeResolve: nodeResolvePlugin(),
+  babel: babelPlugin({
+    cwd: LERNA_ROOT_PATH,
+    runtimeHelpers: true,
+  }),
+}
+
+const getGlobalName = compose(join(""), prepend("Afformative"), map(toPascalCase), tail, split("/"))
+
+const getFileName = compose(join("-"), prepend("afformative"), map(toKebabCase), tail, split("/"))
+
+const { LERNA_PACKAGE_NAME } = process.env
+const PACKAGE_ROOT_PATH = process.cwd()
+const INPUT_FILE = path.join(PACKAGE_ROOT_PATH, "src/index.js")
+
+const globals = {
+  react: "React",
+  "prop-types": "PropTypes",
+}
+
+const globalName = getGlobalName(LERNA_PACKAGE_NAME)
+const fileName = getFileName(LERNA_PACKAGE_NAME)
+
+export default [
+  // NOTE: CJS
+  {
+    input: INPUT_FILE,
+    output: {
+      file: path.join(PACKAGE_ROOT_PATH, "lib", `${fileName}.js`),
+      format: "cjs",
+      indent: false,
+    },
+    plugins: [autoExternal(), plugins.nodeResolve, plugins.babel, plugins.cjs],
+  },
+
+  // NOTE: ES
+  {
+    input: INPUT_FILE,
+    output: {
+      file: path.join(PACKAGE_ROOT_PATH, "es", `${fileName}.js`),
+      format: "es",
+      indent: false,
+    },
+    plugins: [autoExternal(), plugins.nodeResolve, plugins.babel, plugins.cjs],
+  },
+
+  // NOTE: UMD Development
+  {
+    input: INPUT_FILE,
+    external: Object.keys(globals),
+    output: {
+      file: path.join(PACKAGE_ROOT_PATH, "dist", `${fileName}.js`),
+      format: "umd",
+      name: globalName,
+      indent: false,
+      globals,
+    },
+    plugins: [
+      replace({ "process.env.NODE_ENV": JSON.stringify("development") }),
+      plugins.nodeResolve,
+      plugins.babel,
+      plugins.cjs,
+    ],
+  },
+
+  // NOTE: UMD Production
+  {
+    input: INPUT_FILE,
+    external: Object.keys(globals),
+    output: {
+      file: path.join(PACKAGE_ROOT_PATH, "dist", `${fileName}.min.js`),
+      format: "umd",
+      name: globalName,
+      indent: false,
+      globals,
+    },
+    plugins: [
+      replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
+      plugins.nodeResolve,
+      plugins.babel,
+      plugins.cjs,
+      plugins.terser,
+    ],
+  },
+]
